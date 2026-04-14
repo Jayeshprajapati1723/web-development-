888f0ac0ea8f7e7869dc4e92b0e3609df6a47b8e
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-04-14 16:40:48 +0530

    variable in js

---
+++ 43variables/var.js
@@ -0,0 +1,40 @@
+// var const let declarators hote h var bhot purana h or ye hmesah same variable exist k bad bhi permit kr deta redeclare k liye 
+var a= 55 ;
+// var a = 69 ;
+
+// const isme constant rkhna ho hme hr jagah jb use kro 
+const p= 22/7 ;
+
+// let ye global acess h or ye redeclare nhi ho skta h 
+let l = 45 ;
+// never redeclare let l = 23 ;
+
+console.log(a) ;
+console.log((a )) ;
+console.log(a) ;
+console.log((a )) ;
+
+
+
+console.log(p);
+console.log((a )) ;
+
+
+console.log(l) ;
+console.log((a )) ;
+let nam = "jayesh " ;
+console.log((nam )) ;
+
+// user defined data type object h 
+const o= {
+     nm : "jayesh" ,
+     salary : 200000 ,
+     isemployee: true
+}
+
+console.log(typeof(o)) ;
+console.log(o.salary) ;
+
+console.log(o.nm) ;
+console.log(o.isemployee) ;
+
