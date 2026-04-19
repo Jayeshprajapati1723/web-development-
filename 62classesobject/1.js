c0d4ce047f066a982ff53103ab66a84030fb186a
Jayeshprajapati1723 <jayesh.prajapati2024@sait.ac.in>
2026-04-19 17:22:22 +0530

    object and class

---
+++ 62classesobject/1.js
@@ -0,0 +1,27 @@
+// class and object 
+class Animal {
+
+//constructor name not same as class in js 
+    constructor(){
+console.log("object create ")
+    }
+    eat() {
+        console.log("aanimal is eat food ") ;
+    }
+
+    jump() {
+        console.log("animal are jumps ") ;
+    }
+}
+let obj1 =new Animal() ;
+console.log(obj1) ;
+let obj2 = new Animal;
+
+
+// extends using inheritance 
+
+class lion extends Animal {
+    // inheritance 
+}
+
+// whi sb c++ k concepts oops k matlb ki syntax diffrence 
